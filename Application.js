1e4a1d5aa79001dd0e6e23e5f5a0c3bac34d5d97
real wwwdev <sheen3d@yandex.ru>
2023-05-01 00:38:28 +0500

    adding some code

---
+++ Application.js
@@ -0,0 +1,67 @@
+// отвечает за все приложение в целом
+class Application {
+  mouse = null;
+
+  player = null;
+  opponent = null;
+
+  scenes = {};
+  activeScene = null;
+
+  constructor(scenes = {}) {
+    const mouse = new Mouse(document.body);
+
+    const player = new BattlefieldView(true);
+    const opponent = new BattlefieldView(false);
+
+    Object.assign(this, { mouse, player, opponent });
+
+    //поиск div и добавление dom
+    document.querySelector('[data-side="player"]').append(player.root);
+    document.querySelector('[data-side="opponent"]').append(opponent.root);
+
+    //проходимся по всем сценам
+    for (const [sceneName, SceneClass] of Object.entries(scenes)) {
+      //позвоялет работать как с массивом
+      //генерируем сцены, передаем имя и указываем на них
+      this.scenes[sceneName] = new SceneClass(sceneName, this);
+    }
+    //проходимся по всем уже созданным сценам
+    for (const scene of Object.values(this.scenes)) {
+      scene.init();
+    }
+
+    requestAnimationFrame(() => this.tick());
+  }
+
+  tick() {
+    requestAnimationFrame(() => this.tick());
+
+    if (this.activeScene) {
+      this.activeScene.update();
+    }
+
+    this.mouse.tick();
+  }
+  //начать сцену
+  start(sceneName, ...args) {
+    //проверка: есть ли активная сцена
+    if (this.activeScene && this.activeScene.name === sceneName) {
+      return false;
+    }
+    //проверка: на отсутсвие сцены
+    if (!this.scenes.hasOwnProperty(sceneName)) {
+      return false;
+    }
+    //если нужна новая, то останавливаем старую
+    if (this.activeScene) {
+      this.activeScene.stop();
+    }
+    //поиск нужной сцены и ее запуск
+    const scene = this.scenes[sceneName];
+    this.activeScene = scene;
+    scene.start(...args);
+
+    return true;
+  }
+}
