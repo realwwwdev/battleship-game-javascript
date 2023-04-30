1c901072ee9afbfe7def9119e7dd179f71c251a0
real wwwdev <sheen3d@yandex.ru>
2023-05-01 00:44:22 +0500

    adding some code

---
+++ Scene.js
@@ -0,0 +1,17 @@
+// класс соответствует состоянию сцены
+class Scene {
+  name = null;
+  app = null;
+
+  constructor(name, app) {
+    Object.assign(this, { name, app });
+  }
+
+  init() {}
+
+  start() {}
+
+  update() {}
+
+  stop() {}
+}
